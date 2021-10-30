e70ca6b3ad489f1c681b3fffadf95a2382136a34
RenataS Matsumoto <renatas.matsumoto@gmail.com>
2021-10-29 21:51:27 -0300

    versao mockup

--- app.js
+++ app.js
@@ -12,18 +12,33 @@
 
 const app = require('./config/server')
 
+// Importação da base de dados mockup.js
+const noticias = require('./mockup')
+
 // rota home
-app.get('/', function(req,res){
-    res.render('home/index')
+app.get('/', function(req, res){
+    res.render('home/index' ,  {noticias: noticias.slice(0, 3)})
 })
 
 // rota notícias
-app.get('/noticias', function(req,res){
-   res.render('noticias/noticias')
+app.get('/noticias', function(req, res){
+    // vamos passar atravez de um JSON o conteúdo da variável noticias
+    res.render('noticias/noticias', {noticias : noticias})
 })
 
+// rota noticia
+app.get('/noticia', function(req, res){
+    // metodo GEt recurepa o Id da noticia
+    const id = req.query.id
+    res.render('noticias/noticia', {noticia : noticias[id]})
+})
+
+// rota admin
+app.get('/admin', function(req, res){
+    res.render('admin/form_add_noticia')
+})
 
 app.listen(3000, () => {
-    console.log('Servidor rodando com express, http://localhost:3000')
-    console.log('Pressione CTRL+C para encerrar')
+    console.log('Servidor rodando com express')
+    console.log('Pressione CTRL + C para encerrar')
 })
--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,1 +1,14 @@
 // Scripts por Cel.Lep Tech
+
+const url_atual = window.location.pathname
+
+if(url_atual =="/"){
+    document.querySelector('#menuHome').className = 'nav-link text-white active'
+
+}else if(url_atual =="/noticias"){
+    document.querySelector('#menuNoticias').className = 'nav-link text-white active'
+
+} else if(url_atual =="/admin"){
+    document.querySelector('#menuAdmin').className = 'nav-link text-white active'
+
+}
